9305a65839e38c4b0a7f2469983a8bbf965778db
jireve999 <wbajxw999@aliyun.com>
2025-02-21 22:20:36 +0800

    add IPFS

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -1,6 +1,5 @@
 import { ethers } from "ethers";
 import { connect } from '../service/connection-service'
-
 import 'react-notifications-component/dist/theme.css'
 
 function Connect() {
@@ -9,13 +8,9 @@ function Connect() {
   }
   return (
     <div>
-    
-
         <a href="javascript:void(0);" onClick={connectWallet}>
           connect
         </a>
-    
-
     </div>
   )
 }
